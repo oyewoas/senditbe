e68a3cbcc5190aab299bf79e8eedccc5a55269c4
Ayooluwa Oyewo <ayooluwaoyewo@gmail.com>
2019-01-28 00:25:54 +0100

    testing api end points

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import chai from 'chai';
 import bcrypt from 'bcrypt';
 import chaiHttp from 'chai-http';
@@ -5,14 +6,18 @@ import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import server from '../server';
 import pool from '../app/db/pool';
-import createAllTables from '../app/db/db';
+import {
+  createAllTables,
+} from '../app/db/db';
 
 createAllTables();
 
 
 dotenv.config();
 
-const { expect } = chai;
+const {
+  expect,
+} = chai;
 chai.use(chaiHttp);
 
 const title = 'Here is a title';
@@ -22,457 +27,35 @@ const password = 'testingtesting';
 const username = 'test';
 
 // Generating token for testing
-const token = jwt.sign(
-  {
+const token = jwt.sign({
     email: 'test@gmail.com',
     userId: 1,
   },
-  process.env.JWT_KEY,
-  {
+  process.env.JWT_KEY, {
     expiresIn: '1h',
-  },
-);
-
+  });
 beforeEach(() => {
   pool.query('TRUNCATE TABLE users CASCADE',
     (err) => {
       if (err) {
-        // console.log(err);
+        console.log(err);
       }
       //  pool.end;()
     });
   pool.query('TRUNCATE TABLE parcels CASCADE',
     (err) => {
       if (err) {
-        // console.log(err);
+        console.log(err);
       }
       //  pool.end;()
     });
 });
-// Testing the Get all entries
-describe('/GET entries', () => {
-  it('it should not GET all the entries when there is no auth token', (done) => {
-    chai.request(server)
-      .get('/api/v1/entries')
-      .end((err, res) => {
-        // console.log(JSON.stringify(res.body, undefined, 3));
-        expect(res.body.message).equal('Auth failed');
-        expect(res.status).equal(401);
-        done();
-      });
-  });
-  it('it should not GET entries when empty', (done) => {
-    chai.request(server)
-      .get('/api/v1/entries')
-      .set('Authorization', `Bearer ${token}`)
-      .end((err, res) => {
-        // console.log(JSON.stringify(res.body, undefined, 3));
-        expect(res.status).equal(404);
-        expect(res.body.message).equal('There is no entries yet');
-        done();
-      });
-  });
-  it('it should GET all the entries', (done) => {
-    pool.query('INSERT INTO users(email, password, username, user_id) values($1, $2, $3, $4)', [email, password, username, 1], (err) => {
-      if (err) {
-        // console.log(err);
-      } else {
-        // console.log(result);
-        pool.query('INSERT INTO entries(title, description, user_id) values($1, $2, $3)', [title, description, 1], () => {
-          chai.request(server)
-            .get('/api/v1/entries')
-            .set('Authorization', `Bearer ${token}`)
-            .end((error, res) => {
-              // console.log(JSON.stringify(res.body, undefined, 3));
-              expect(res.body.data.entries).to.be.an('Array');
-              expect(res.status).equal(200);
-              expect(res.body.data.size).equal(1);
-              done();
-            });
-        });
-      }
-    });
-  });
-});
-// Test for an invalid post request
-describe('/POST entry', () => {
-  it('it should not POST an entry without text field', (done) => {
-    const entry = {
-      title: 'Today was Hectic',
-    };
-    chai.request(server)
-      .post('/api/v1/entries')
-      .send(entry)
-      .set('Authorization', `Bearer ${token}`)
-      .end((err, res) => {
-        expect(res.body.message).equals('Bad Request');
-        expect(res.body.status).equals(400);
-        done();
-      });
-  });
-
-  it('it should not POST an entry without title field', (done) => {
-    const entry = {
-      text: 'Today was Hectic',
-    };
-    chai.request(server)
-      .post('/api/v1/entries')
-      .send(entry)
-      .set('Authorization', `Bearer ${token}`)
-      .end((err, res) => {
-        expect(res.body.message).equals('Bad Request');
-        expect(res.body.status).equals(400);
-        done();
-      });
-  });
-
-  it('it should not POST an entry with empty text field', (done) => {
-    const entry = {
-      title: 'Today was Hectic',
-      text: '',
-    };
-    chai.request(server)
-      .post('/api/v1/entries')
-      .send(entry)
-      .set('Authorization', `Bearer ${token}`)
-      .end((err, res) => {
-        expect(res.body.message).equals('Bad Request');
-        expect(res.body.status).equals(400);
-        done();
-      });
-  });
-
-  it('it should not POST an entry with empty title field', (done) => {
-    const entry = {
-      text: 'Today was Hectic',
-      title: '',
-    };
-    chai.request(server)
-      .post('/api/v1/entries')
-      .send(entry)
-      .set('Authorization', `Bearer ${token}`)
-      .end((err, res) => {
-        expect(res.body.message).equals('Bad Request');
-        expect(res.body.status).equals(400);
-        done();
-      });
-  });
-
-  it('it should not POST an entry with empty title and text field', (done) => {
-    const entry = {
-      text: '',
-      title: '',
-    };
-    chai.request(server)
-      .post('/api/v1/entries')
-      .send(entry)
-      .set('Authorization', `Bearer ${token}`)
-      .end((err, res) => {
-        expect(res.body.message).equals('Bad Request');
-        expect(res.body.status).equals(400);
-        done();
-      });
-  });
-
-  it('it should not POST an entry when there is no user', (done) => {
-    const entry = {
-      text: 'Today was Hectic',
-      title: 'So that was how my day went, hectic as ...',
-    };
-    chai.request(server)
-      .post('/api/v1/entries')
-      .send(entry)
-      .set('Authorization', `Bearer ${token}`)
-      .end((err, res) => {
-        expect(res.body.message).equals('Internal Server Error');
-        expect(res.body.description).equals('Could not post entry');
-        expect(res.body.status).equals('500');
-        expect(res.status).equals(500);
-        done();
-      });
-  });
-
-  it('it should  POST an entry when there is a user', (done) => {
-    const entry = {
-      text: 'Today was Hectic',
-      title: 'So that was how my day went, hectic as ...',
-    };
-    pool.query('INSERT INTO users(email, password, username, user_id) values($1, $2, $3, $4)', [email, password, username, 1], (err) => {
-      if (err) {
-        // console.log(err);
-      } else {
-        chai.request(server)
-          .post('/api/v1/entries')
-          .send(entry)
-          .set('Authorization', `Bearer ${token}`)
-          .end((error, res) => {
-            expect(res.body.message).equals('Entry Created successfully');
-            expect(res.body.data.entries).to.be.an('Array');
-            expect(res.body.status).equals('201');
-            expect(res.status).equals(201);
-            done();
-          });
-      }
-    });
-  });
-  it('it should not POST an entry when there is no auth token', (done) => {
-    const entry = {
-      text: 'Today was Hectic',
-      title: 'So that was how my day went, hectic as ...',
-    };
-    chai.request(server)
-      .post('/api/v1/entries')
-      .send(entry)
-      .end((err, res) => {
-        // console.log(JSON.stringify(res.body, undefined, 3));
-        expect(res.body.message).equal('Auth failed');
-        expect(res.status).equal(401);
-        done();
-      });
-  });
-});
-// Testing for getting an entry by id
-describe('/GET/:id entry', () => {
-  it('it should not GET an entry by id if there is no auth token', (done) => {
-    const id = 1000;
-    chai.request(server)
-      .get(`/api/v1/entries/${id}`)
-      .end((err, res) => {
-        expect(res.body.message).equals('Auth failed');
-        expect(res.status).equals(401);
-        done();
-      });
-  });
-
-  it('it should not GET an entry by id if entry does not exist', (done) => {
-    const id = 1000;
-    chai.request(server)
-      .get(`/api/v1/entries/${id}`)
-      .set('Authorization', `Bearer ${token}`)
-      .end((err, res) => {
-        expect(res.body.message).equals('Entry Not Found');
-        expect(res.status).equals(404);
-        done();
-      });
-  });
-
-  it('it should GET an entry by id', (done) => {
-    pool.query('INSERT INTO users(email, password, username, user_id) values($1, $2, $3, $4)', [email, password, username, 1], (err) => {
-      if (err) {
-        // console.log(err);
-      } else {
-        // console.log(result);
-        pool.query('INSERT INTO entries(title, description, user_id, id) values($1, $2, $3, $4)', [title, description, 1, 1], () => {
-          const id = 1;
-          chai.request(server)
-            .get(`/api/v1/entries/${id}`)
-            .set('Authorization', `Bearer ${token}`)
-            .end((error, res) => {
-              // console.log(JSON.stringify(res.body, undefined, 3));
-              expect(res.body.entry).to.be.an('Object');
-              expect(res.status).equal(200);
-              expect(res.body.message).equal('Entry returned successfully');
-              done();
-            });
-        });
-      }
-    });
-  });
-});
-
-
-// Testing of updating of an entry
-describe('/PUT/:id entry', () => {
-  const entryUpdated = {
-    text: 'Today was Hectic',
-    title: 'This is the updated title',
-  };
-  it('it should not UPDATE an entry without text field', (done) => {
-    const entry = {
-      title: 'Today was Hectic',
-    };
-    const id = 1000;
-    chai.request(server)
-      .put(`/api/v1/entries/${id}`)
-      .send(entry)
-      .set('Authorization', `Bearer ${token}`)
-      .end((err, res) => {
-        expect(res.body.message).equals('Bad Request');
-        expect(res.body.status).equals(400);
-        done();
-      });
-  });
-
-  it('it should not UPDATE an entry without title field', (done) => {
-    const entry = {
-      text: 'Today was Hectic',
-    };
-    const id = 1000;
-    chai.request(server)
-      .put(`/api/v1/entries/${id}`)
-      .send(entry)
-      .set('Authorization', `Bearer ${token}`)
-      .end((err, res) => {
-        expect(res.body.message).equals('Bad Request');
-        expect(res.body.status).equals(400);
-        done();
-      });
-  });
-
-  it('it should not UPDATE an entry with empty text field', (done) => {
-    const entry = {
-      title: 'Today was Hectic',
-      text: '',
-    };
-    const id = 1000;
-    chai.request(server)
-      .put(`/api/v1/entries/${id}`)
-      .send(entry)
-      .set('Authorization', `Bearer ${token}`)
-      .end((err, res) => {
-        expect(res.body.message).equals('Bad Request');
-        expect(res.body.status).equals(400);
-        done();
-      });
-  });
-
-  it('it should not UPDATE an entry with empty title field', (done) => {
-    const entry = {
-      text: 'Today was Hectic',
-      title: '',
-    };
-    const id = 1000;
-    chai.request(server)
-      .put(`/api/v1/entries/${id}`)
-      .send(entry)
-      .set('Authorization', `Bearer ${token}`)
-      .end((err, res) => {
-        expect(res.body.message).equals('Bad Request');
-        expect(res.body.status).equals(400);
-        done();
-      });
-  });
-
-  it('it should not UPDATE an entry with empty title and text field', (done) => {
-    const entry = {
-      text: '',
-      title: '',
-    };
-    const id = 1000;
-    chai.request(server)
-      .put(`/api/v1/entries/${id}`)
-      .send(entry)
-      .set('Authorization', `Bearer ${token}`)
-      .end((err, res) => {
-        expect(res.body.message).equals('Bad Request');
-        expect(res.body.status).equals(400);
-        done();
-      });
-  });
-
-  it('it should not UPDATE an entry by id if there is no auth token', (done) => {
-    const id = 1000;
-    chai.request(server)
-      .put(`/api/v1/entries/${id}`)
-      .send(entryUpdated)
-      .end((err, res) => {
-        expect(res.body.message).equals('Auth failed');
-        expect(res.status).equals(401);
-        done();
-      });
-  });
-
-  // it('it should POST an entry by id if entry does not exist while updating', (done) => {
-  //   const id = 1000;
-  //   chai.request(server)
-  //     .put(`/api/v1/entries/${id}`)
-  //     .set('Authorization', `Bearer ${token}`)
-  //     .send(entryUpdated)
-  //     .end((err, res) => {
-  //       expect(res.body.message).equals('Entry Modified successfully');
-  //       expect(res.status).equals(200);
-  //       done();
-  //     });
-  // });
-
-  it('it should UPDATE an entry by id', (done) => {
-    pool.query('INSERT INTO users(email, password, username, user_id) values($1, $2, $3, $4)', [email, password, username, 1], (err) => {
-      if (err) {
-        // console.log(err);
-      } else {
-        // console.log(result);
-        pool.query('INSERT INTO entries(title, description, user_id, id) values($1, $2, $3, $4)', [title, description, 1, 1], () => {
-          const id = 1;
-          chai.request(server)
-            .put(`/api/v1/entries/${id}`)
-            .set('Authorization', `Bearer ${token}`)
-            .send(entryUpdated)
-            .end((error, res) => {
-              // console.log(JSON.stringify(res.body, undefined, 3));
-              expect(res.body.data).to.be.an('Object');
-              expect(res.status).equal(200);
-              expect(res.body.message).equal('Entry Modified successfully');
-              done();
-            });
-        });
-      }
-    });
-  });
-});
-//
-describe('/DELETE/:id entry', () => {
-  it('it should not DELETE an entry by id if there is no auth token', (done) => {
-    const id = 1000;
-    chai.request(server)
-      .delete(`/api/v1/entries/${id}`)
-      .end((err, res) => {
-        expect(res.body.message).equals('Auth failed');
-        expect(res.status).equals(401);
-        done();
-      });
-  });
-
-  it('it should not DELETE an entry by id if entry does not exist', (done) => {
-    const id = 1000;
-    chai.request(server)
-      .delete(`/api/v1/entries/${id}`)
-      .set('Authorization', `Bearer ${token}`)
-      .end((err, res) => {
-        expect(res.body.message).equals('Entry does not exist');
-        expect(res.status).equals(200);
-        done();
-      });
-  });
-  it('it should DELETE an entry by id', (done) => {
-    pool.query('INSERT INTO users(email, password, username, user_id) values($1, $2, $3, $4)', [email, password, username, 1], (err) => {
-      if (err) {
-        // console.log(err);
-      } else {
-        // console.log(result);
-        pool.query('INSERT INTO entries(title, description, user_id, id) values($1, $2, $3, $4)', [title, description, 1, 1], () => {
-          const id = 1;
-          chai.request(server)
-            .delete(`/api/v1/entries/${id}`)
-            .set('Authorization', `Bearer ${token}`)
-            .end((error, res) => {
-              // console.log(JSON.stringify(res.body, undefined, 3));
-              expect(res.body.data).to.be.an('Object');
-              expect(res.status).equal(200);
-              expect(res.body.message).equal('Entry Deleted successfully');
-              done();
-            });
-        });
-      }
-    });
-  });
-});
-
 
 // Authentication
 describe('/POST new user', () => {
   it('it should not CREATE a user without email or password field only', (done) => {
     const user = {
-      username: 'phemmelliot',
+      username: 'ayooluwa',
     };
     chai.request(server)
       .post('/api/v1/user/signup')
@@ -514,7 +97,7 @@ describe('/POST new user', () => {
 
   it('it should not CREATE a user with empty email or password field only', (done) => {
     const user = {
-      username: 'phemmelliot',
+      username: 'ayooluwa',
       password: '',
       email: '',
     };
@@ -564,9 +147,9 @@ describe('/POST new user', () => {
     const user = {
       email: 'test@gmail.com',
       password: 'password',
-      username: 'phemmelliot',
+      username: 'ayooluwa',
     };
-    pool.query('INSERT INTO users(email, password, username) values($1, $2, $3)', ['test@gmail.com', 'password', 'phemmelliot'], () => {
+    pool.query('INSERT INTO users(email, password, username) values($1, $2, $3)', ['test@gmail.com', 'password', 'ayooluwa'], () => {
       chai.request(server)
         .post('/api/v1/user/signup')
         .send(user)
@@ -583,7 +166,7 @@ describe('/POST new user', () => {
     const user = {
       email: 'test.com',
       password: 'password',
-      username: 'phemmelliot',
+      username: 'ayooluwa',
     };
     chai.request(server)
       .post('/api/v1/user/signup')
@@ -599,7 +182,7 @@ describe('/POST new user', () => {
     const user = {
       email: 'test.com',
       password: 'pass',
-      username: 'phemmelliot',
+      username: 'ayooluwa',
     };
     chai.request(server)
       .post('/api/v1/user/signup')
@@ -615,7 +198,7 @@ describe('/POST new user', () => {
     const user = {
       email: 'test@gmail.com',
       password: '         ',
-      username: 'phemmelliot',
+      username: 'ayooluwa',
     };
     chai.request(server)
       .post('/api/v1/user/signup')
@@ -631,7 +214,7 @@ describe('/POST new user', () => {
     const user = {
       email: 'test@gmail.com',
       password: 'password',
-      username: 'phemmelliot',
+      username: 'ayooluwa',
     };
     chai.request(server)
       .post('/api/v1/user/signup')
@@ -759,7 +342,7 @@ describe('/POST Log user in', () => {
     let logInpassword;
     bcrypt.hash('password', 10, (error, hash) => {
       logInpassword = hash;
-      pool.query('INSERT INTO users(email, password, username) values($1, $2, $3)', ['test@gmail.com', logInpassword, 'phemmelliot'], () => {
+      pool.query('INSERT INTO users(email, password, username) values($1, $2, $3)', ['test@gmail.com', logInpassword, 'ayooluwa'], () => {
         chai.request(server)
           .post('/api/v1/user/login')
           .send(user)
