d9466727ef24fd4b360c181dc5c6ccfab7424b1f
Ayooluwa Oyewo <ayooluwaoyewo@gmail.com>
2019-03-05 15:28:29 +0100

    still working on using express router

--- app/routes/parcelRoutes.js
+++ app/routes/parcelRoutes.js
@@ -1,3 +1,5 @@
+import express from 'express';
+
 import { 
   createParcel,
   getAllParcelOrders,
@@ -8,31 +10,25 @@ import {
 } from '../controller/parcelsController';
 import verifyAuth from '../middlewares/verifyAuth';
 
+
+
 export default function route(app) {
   // const badRequest = { status: 400, message: 'Bad Request' };
+    // Get Request for a single Parcel
+    app.get('/parcels/:id', verifyAuth, getAparcel)
+    // Delete Request to delete A parcel
+    app.patch('/parcels/:id/cancel', verifyAuth, cancelParcelOrder)
+    // Change parcel destination
+    app.patch('/parcels/:id/destination', verifyAuth, updateParcelDestination);
+  
+  // Get request for all Parcels order by a user 
+    app.get('/users/parcels', verifyAuth, getAllParcelforUser);
 
-  // Get Request for a single Parcel
-  app.get('/api/v1/parcels/:id', verifyAuth, getAparcel);
-
-
-  // // Get request for all Parcels order by a user
-  app.get('/api/v1/users/parcels', verifyAuth, getAllParcelforUser);
-
-
-  // // Post Request for a parcel entry
-  app.post('/api/v1/parcels', verifyAuth, createParcel);
-
-  // Delete Request to delete A parcel
-  app.patch('/api/v1/parcels/:id/cancel', verifyAuth, cancelParcelOrder);
-
-  app.patch('/api/v1/parcels/:id/destination', verifyAuth, updateParcelDestination);
+  // Post Request for a parcel entry
+    app.post('/parcels',verifyAuth, createParcel);
 
   // Fetch all parcel delivery orders
   // app.get('/api/v1/parcels', getAllParcelOrders);
 
-
-
-  // // Put Request to modify the content of an entryRoutes
-  // app.put('/api/v1/entries/:id', checkAuth, updateEntry);
-
 }
+
